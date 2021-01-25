6b217d23a1b6759aab8bb71fbd60aa7ddadc3a77
sh1hab <shihab.gsc10@gmail.com>
2021-01-26 00:38:24 +0600

    profile edit

--- myapp/routes.py
+++ myapp/routes.py
@@ -6,6 +6,7 @@ from werkzeug.urls import url_parse
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from datetime import datetime
+from myapp.forms import EditProfileForm
 
 
 @application.before_request
@@ -89,3 +90,18 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
+
+@application.route('/edit_profile', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.about_me = form.about_me.data
+        db.session.commit()
+        flash('Your changes has been saved')
+        return redirect(url_for('edit_profile'))
+    elif request.method=='GET':
+        form.username.data = current_user.username
+        form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html', title='edit profile', form=form)
